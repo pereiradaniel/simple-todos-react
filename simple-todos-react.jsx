cc34dd1123692b0f61bba2b575497610720e1779
Daniel <truenorthtrainingcompany@gmail.com>
2016-02-02 14:10:51 -0500

    Add extra security to methods Only owner can delete or check off a private task Private tasks are not SUPPOSED to be viewable by non-owners.

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -39,10 +39,20 @@ Meteor.methods({
 	},
 
 	removeTask(taskId) {
+		const task = Tasks.findOne(taskId);
+		if (task.private && task.owner !== Meteor.userId()) {
+			throw new Meteor.Error("not-authorized");
+		}
+
 		Tasks.remove(taskId);
 	},
 
 	setChecked(taskId, setChecked) {
+		const task = Tasks.findOne(taskId);
+		if (task.private && task.owner !== Meteor.userId()) {
+			throw new Meteor.Error("not-authorized");
+		}
+		
 		Tasks.update(taskId, { $set: { checked: setChecked} });
 	},
 
